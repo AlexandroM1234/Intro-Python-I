1fd8339b0a9737daef388cda9cbfd96c8def03ac
Alex Martinez <alexmartinez0071234@gmail.com>
2020-07-06 17:44:19 -0400

    finished 6-8

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,14 +10,13 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = []
-
+y = [i for i in range(1,6)]
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [i**3 for i in range(10)]
 
 print(y)
 
@@ -26,7 +25,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [values.upper() for values in a]
 
 print(y)
 
@@ -37,5 +36,11 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
+for values in x:
+    values=int(values)
+    if (values%2==0):
+        y.append(values)
+    else:
+        print("values cannot be odd")
 
 print(y)
