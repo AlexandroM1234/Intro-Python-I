ca5bd1dbc111f28dfdc776a6abc85b647d7fe758
Alex Martinez <alexmartinez0071234@gmail.com>
2020-07-07 15:53:40 -0400

    finished scopes and workling on other files

--- src/11_args.py
+++ src/11_args.py
@@ -26,7 +26,6 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-a=tuple(a)
 print(f2(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
@@ -35,6 +34,12 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num1,num2=0):
+    if(num1 and num2):
+        return num1 + num2
+    else:
+        return num1 + 1
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -49,6 +54,7 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4():
 
 # Should print
 # key: a, value: 12
